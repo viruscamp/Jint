31f2788addf0069d481be6dfa3f0c83527748d20
S?bastien Ros <S?bastien Ros@localhost>
2010-09-19 09:26:48 -0700

    Small refactoring on JsMathConstructor

--- Jint/Native/JsMathConstructor.cs
+++ Jint/Native/JsMathConstructor.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Jint.Delegates;
 
 namespace Jint.Native
@@ -16,24 +14,24 @@ namespace Jint.Native
             var random = new Random();
 
             #region Functions
-            this["abs"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Abs(d))));
-            this["acos"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Acos(d))));
-            this["asin"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Asin(d))));
-            this["atan"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Atan(d))));
+            this["abs"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Abs(d))));
+            this["acos"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Acos(d))));
+            this["asin"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Asin(d))));
+            this["atan"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Atan(d))));
             this["atan2"] = new ClrFunction(new Func<double, double, JsNumber>((y, x) => Global.NumberClass.New(Math.Atan2(y, x))));
-            this["ceil"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Ceiling(d))));
-            this["cos"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Cos(d))));
-            this["exp"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Exp(d))));
-            this["floor"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Floor(d))));
-            this["log"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Log(d))));
+            this["ceil"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Ceiling(d))));
+            this["cos"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Cos(d))));
+            this["exp"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Exp(d))));
+            this["floor"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Floor(d))));
+            this["log"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Log(d))));
             this["max"] = new ClrFunction(new Func<double, double, JsNumber>((a, b) => Global.NumberClass.New(Math.Max(a, b))));
             this["min"] = new ClrFunction(new Func<double, double, JsNumber>((a, b) => Global.NumberClass.New(Math.Min(a, b))));
             this["pow"] = new ClrFunction(new Func<double, double, JsNumber>((a, b) => Global.NumberClass.New(Math.Pow(a, b))));
             this["random"] = global.FunctionClass.New(new Func<double>(random.NextDouble));
-            this["round"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Round(d))));
-            this["sin"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Sin(d))));
-            this["sqrt"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Sqrt(d))));
-            this["tan"] = new ClrFunction(new Func<double, JsNumber>((d) => Global.NumberClass.New(Math.Tan(d))));
+            this["round"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Round(d))));
+            this["sin"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Sin(d))));
+            this["sqrt"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Sqrt(d))));
+            this["tan"] = new ClrFunction(new Func<double, JsNumber>(d => Global.NumberClass.New(Math.Tan(d))));
             #endregion
 
             this["E"] = global.NumberClass.New(Math.E);

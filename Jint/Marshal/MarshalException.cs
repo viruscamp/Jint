4bf8bbb8ede1b2819ef1cc43c67d5557474b6729
cin <cin@localhost>
2010-11-15 00:45:07 +0300

    Working on interoperation temp commit

---
+++ Jint/Marshal/MarshalException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jint.Marshal
+{
+    class MarshalException: JintException
+    {
+        MarshalException()
+            : base()
+        {
+        }
+
+        MarshalException(string msg)
+            : base(msg)
+        {
+        }
+
+        MarshalException(string msg, Exception inner)
+            : base(msg, inner)
+        {
+        }
+    }
+}

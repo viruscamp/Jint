c8c59f39b2d87c6b588ad643acc206959c9930e4
cin <cin@localhost>
2010-11-16 07:52:22 +0300

    temp commit

--- Jint/Native/ClrOverload.cs
+++ Jint/Native/ClrOverload.cs
@@ -11,14 +11,58 @@ namespace Jint.Native
 
         Dictionary<string, JsMethodImpl> m_cache = new Dictionary<string, JsMethodImpl>();
 
+        struct MethodMatch
+        {
+            public MethodInfo method;
+            public int weight;
+            public Type[] parameters;
+        }
+
+        // a list of methods
         LinkedList<MethodInfo> m_methods = new LinkedList<MethodInfo>();
+        
+        // a list of generics which parameters could be suggested from passed arguments
         LinkedList<MethodInfo> m_autoGenerics = new LinkedList<MethodInfo>();
+        
+        // a list of generucs which requires explicit parameters
         LinkedList<MethodInfo> m_manualGenerics = new LinkedList<MethodInfo>();
         
 
         public ClrOverload(ICollection<MethodInfo> methods , JsObject prototype)
             : base(prototype)
         {
+        }
+
+        MethodInfo MatchMethod(Type[] agrs, Type[] genericArguments)
+        {
+            LinkedList<MethodMatch> matches = new LinkedList<MethodMatch>();
+            if (genericArguments != null && genericArguments.Length > 0)
+            {
+                // we have an explicit generic parameters
+                foreach (var list in new ICollection<MethodInfo>[] { m_autoGenerics, m_manualGenerics })
+                {
+                    foreach (var item in list)
+                    {
+                        MethodMatch match = new MethodMatch() { weight = 0 };
+                        if (item.GetGenericArguments().Length == genericArguments.Length && item.GetParameters().Length == agrs.Length)
+                        {
+                            try
+                            {
+                                match.method = item.MakeGenericMethod(genericArguments);
+                                match.parameters = Array.ConvertAll<ParameterInfo, Type>(match.method.GetParameters(), pi => pi.ParameterType);
+                                matches.AddLast(match);
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+
+            }
         }
 
         string MakeKey(Type[] types)

bda5cf59eb61ca15a8c76e78f6f6930dccd549d7
S?bastien Ros <S?bastien Ros@localhost>
2011-02-20 14:35:01 -0800

    Calling a non-function should throw an error
    Work Items: 6888

--- Jint.Tests/Fixtures.cs
+++ Jint.Tests/Fixtures.cs
@@ -1591,6 +1591,20 @@ var fakeButton = new Test.FakeButton();");
             ");
         }
 
+        [TestMethod]
+        public void CallingANonMethodShouldThrowAnException() {
+            Test(@"
+                try {
+                    var x = { prop: 'abc'};
+                    x.prop();
+                    fail('should have thrown an Error');
+                }
+                catch (e) {
+                    return;
+                }
+                fail('should have caught an Error');
+            ");
+        }
     }
 
     public struct Size {

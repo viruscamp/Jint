c2f0e15a47874629a39c183bcb81fe2e26aa2088
cin <cin@localhost>
2011-04-11 20:05:23 +0400

    rewriting descriptors

--- Jint/Native/JsObjectBase.cs
+++ Jint/Native/JsObjectBase.cs
@@ -40,6 +40,25 @@ namespace Jint.Native {
             prototype.ChildNotify();
         }
 
+        void internalDefineOwnProperty(Descriptor current, Descriptor desc) {
+            Debug.Assert(desc.DescriptorType != DescriptorType.Generic);
+
+            if (current != null && m_hasChidren)
+                current.Owner.RepopulateProperty(current.Name);
+
+            if (desc.DescriptorType == DescriptorType.Generic)
+                desc = ((GenericDescriptor)desc).ToValueDescriptor();
+
+            m_properties.Put(desc.Name, desc);
+        }
+
+        protected static bool Reject(string message, bool throwError) {
+            if (throwError)
+                throw new JsTypeException(message);
+            else
+                return false;
+        }
+
         #region IJsObject Members
 
         public IJsObject Prototype {
@@ -75,11 +94,12 @@ namespace Jint.Native {
             // this is own writable property or writable accessor
             if (d != null) {
                 if (d.Owner == this) {
+                    
                     if (d.Writable)
                         d.Set(this, value);
-                    else if (throwError)
-                        throw new JsTypeException(String.Format("A property {0} isn't writable", name));
-                    return;
+                    else
+                        Reject(String.Format("A property {0} isn't writable", name),throwError);
+
                 } else if (d.DescriptorType == DescriptorType.Accessor) {
                     if (d.Writable) {
                         d.Set(this, value);
@@ -90,9 +110,10 @@ namespace Jint.Native {
 
             // the property is absent or inherited value property
             if (!m_extensible)
-                throw new JsTypeException(String.Format("Can't define a new own property {0}, object isn't extensible", name));
+                Reject(String.Format("Can't define a new own property {0}, object isn't extensible", name),throwError);
 
-            DefineOwnProperty(new ValueDescriptor(this, name, value));
+            // optimized version of DefineOwnProperty
+            internalDefineOwnProperty(d,new ValueDescriptor(this, name, value));
         }
 
         // 8.12.4
@@ -167,15 +188,11 @@ namespace Jint.Native {
             if (d == null)
                 return true;
 
-            if (!d.Configurable) {
-                if (throwError)
-                    throw new JsTypeException(String.Format("A property {0} isn't configurable", name));
-                else
-                    return false;
-            }
+            if (!d.Configurable)
+                return Reject(String.Format("A property {0} isn't configurable", name),throwError);
 
             m_properties.Delete(d.Name); // remove from object
-            d.Delete(); // mark as asolete
+            d.Delete(); // mark as absolete
         }
 
         public abstract IJsObject DefaultValue();
@@ -183,6 +200,28 @@ namespace Jint.Native {
         public abstract IJsObject DefaultValue(DefaultValueHints hint);
 
         // 8.12.9
+        /// <summary>
+        /// Implementation of ecma 262.5 8.12.9
+        /// </summary>
+        /// <param name="desc">A new property descriptor</param>
+        /// <param name="throwError">If true, this method will throw an exception in case of error</param>
+        /// <returns>True on success, false otherwise</returns>
+        /// <remarks>
+        /// <para>
+        /// Only descriptors with <c>DescriptorType</c> <c>DescriptorType.Generic</c> can be used to update
+        /// existing descriptors, all other descriptors will replace current once.
+        /// </para>
+        /// <para>
+        /// If a descriptor can't be updated, than merge should return false and the descriptor will be
+        /// replaced by the new descriptor which will be created by calling <c>IGenericDescriptor.ToRealDescritor</c>.
+        /// If Configurable attribute of the original descriptor is false, then operation will be rejected.
+        /// </para>
+        /// <para>
+        /// A <c>Merge</c> method of the descriptor should also check the own Configurable property to decide
+        /// whether to apply or reject modifications. In case of when mofications are rejected the descriptor
+        /// should stay in its original state.
+        /// </para>
+        /// </remarks>
         public bool DefineOwnProperty(Descriptor desc, bool throwError) {
             if (desc == null)
                 throw new ArgumentNullException("desc");
@@ -192,58 +231,28 @@ namespace Jint.Native {
             Descriptor current = m_properties.Get(desc.Name);
 
             if (current == null || current.Owner != this) {
+                // we haven't own property
                 if (!m_extensible) {
-                    if (throwError)
-                        throw new JsTypeException(String.Format("Can't define a new own property {0}, object isn't extensible", desc.Name));
-                    else
-                        return false;
+                    // object isn't extensible
+                    return Reject(String.Format("Can't define a new own property {0}, object isn't extensible", desc.Name), throwError);
                 } else {
-                    if (current != null && m_hasChidren)
-                        current.Owner.RepopulateProperty(current.Name);
-
-                    if (desc.DescriptorType == DescriptorType.Generic)
-                        desc = ((GenericDescriptor)desc).ToValueDescriptor();
- 
-                    m_properties.Put(desc.Name, desc);
+                    internalDefineOwnProperty(
+                        current,
+                        desc.DescriptorType == DescriptorType.Generic ?
+                            ((IGenericDescriptor)desc).ToRealDescriptor() :
+                            desc
+                    );
                 }
             } else {
-                // own property, need to update current descriptor
-                if (!current.Configurable) {
-                    if (
-                        (current.Configurable != desc.Configurable) ||
-                        (current.Enumerable != desc.Enumerable) ||
-                        (current.Writable != desc.Writable) ||
-                        (current.GetType() != desc.GetType())
-                    ) {
-                        if (throwError)
-                            throw new JsTypeException(String.Format("Can't update a property {0}, it's not configurable", desc.Name));
-                        else
-                            return false;
-                    }
-
-                    // for non configurable properties allowed to update only value (if not readonly)
-                    if (current.DescriptorType == DescriptorType.Value) {
-                        IJsObject newValue = desc.Get(this);
-                        IJsObject oldValue = current.Get(this);
-
-                        if (!newValue.Equals(oldValue)) {
-                            if (current.Writable == false) {
-                                if (throwError)
-                                    throw new JsTypeException(String.Format("Can't update a property {0}, it's not writable", desc.Name));
-                                else
-                                    return false;
-                            } else {
-                                current.Set(this, newValue);
-                            }
-                        }
-                    }
-
+                if (desc.DescriptorType != DescriptorType.Generic) {
+                    if (!current.Configurable)
+                        return Reject(String.Format("Can't update a property {0}, it's not configurable", desc.Name), throwError);
                 } else {
-                    if (current.GetType() == desc.GetType() || desc.DescriptorType ==DescriptorType.Generic )
-                        current.Merge(desc);
-                    else {
-                        current.Delete();
-                        m_properties.Put(desc.Name, desc);
+                    if (!current.Merge(desc)) {
+                        if (!current.Configurable)
+                            return Reject(String.Format("Can't update a property {0}, it's not configurable", desc.Name), throwError);
+                        else
+                            internalDefineOwnProperty(current,((IGenericDescriptor)desc).ToRealDescriptor(true,current.Enumerable));
                     }
                 }
             }
--- Jint/Native/ValueDescriptor.cs
+++ Jint/Native/ValueDescriptor.cs
@@ -12,7 +12,7 @@ namespace Jint.Native {
             Configurable = true;
         }
 
-        IJsInstance value;
+        IJsInstance m_value;
 
         public ValueDescriptor(JsObjectBase owner, string name, IJsInstance value)
             : this(owner, name) {
@@ -24,7 +24,7 @@ namespace Jint.Native {
         }
 
         public override Descriptor Clone() {
-            return new ValueDescriptor(Owner, Name, value) {
+            return new ValueDescriptor(Owner, Name, m_value) {
                 Enumerable = this.Enumerable,
                 Configurable = this.Configurable,
                 Writable = this.Writable
@@ -32,17 +32,56 @@ namespace Jint.Native {
         }
 
         public override IJsInstance Get(JsObjectBase that) {
-            return value ?? JsUndefined.Instance;
+            return m_value ?? JsUndefined.Instance;
         }
 
         public override void Set(JsObjectBase that, IJsInstance value) {
             if (!Writable)
                 throw new JintException("This property is not writable");
-            this.value = value;
+            this.m_value = value;
+        }
+
+        public IJsInstance Value {
+            get { return m_value; }
+            set {
+                m_value = value;
+            }
         }
 
         internal override DescriptorType DescriptorType {
             get { return DescriptorType.Value; }
+        }
+
+        public override bool Merge(Descriptor desc) {
+            if (desc == null)
+                throw new ArgumentNullException("desc");
+
+            bool canConfigure = Configurable;
+            bool canWrite = Writable;
+
+            if (!base.Merge(desc))
+                return false;
+
+            canWrite = canWtite || Writable;
+
+            GenericDescriptor gen = desc as GenericDescriptor;
+            
+            if (gen == null)
+                return false;
+
+            if (gen.HasAttribute(DescriptorAttributes.Writable)) {
+                if (canConfigure)
+                    Writable = gen.Writable;
+                else
+                    return false;
+            }
+
+            if (gen.HasAttribute(DescriptorAttributes.Value) && gen.Value != Value) {
+                if (canConfigure || Writable)
+                    Value = gen.Value;
+                else
+                    return false;
+            }
         }
     }
 }

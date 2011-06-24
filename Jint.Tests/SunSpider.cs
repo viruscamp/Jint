4be5d0ca0c7f8ccb678ee36133e4835d55185b40
Sebastien Ros <Sebastien Ros@localhost>
2011-06-23 21:27:43 -0700

    Fixing encodeURIComponent case

--- Jint.Tests/SunSpider.cs
+++ Jint.Tests/SunSpider.cs
@@ -59,8 +59,7 @@ namespace Jint.Tests {
 
         [TestMethod]
         public void TestPerformance() {
-            string[] tests = { "3d-raytrace" };
-            // string[] tests = { "3d-cube", "3d-morph", "3d-raytrace", "access-binary-trees", "access-fannkuch", "access-nbody", "access-nsieve", "bitops-3bit-bits-in-byte", "bitops-bits-in-byte", "bitops-bitwise-and", "bitops-nsieve-bits", "controlflow-recursive", "crypto-aes", "crypto-md5", "crypto-sha1", "date-format-tofte", "date-format-xparb", "math-cordic", "math-partial-sums", "math-spectral-norm", "regexp-dna", "string-base64", "string-fasta", "string-tagcloud", "string-unpack-code", "string-validate-input" };
+            string[] tests = { "3d-cube", "3d-morph", "3d-raytrace", "access-binary-trees", "access-fannkuch", "access-nbody", "access-nsieve", "bitops-3bit-bits-in-byte", "bitops-bits-in-byte", "bitops-bitwise-and", "bitops-nsieve-bits", "controlflow-recursive", "crypto-aes", "crypto-md5", "crypto-sha1", "date-format-tofte", "date-format-xparb", "math-cordic", "math-partial-sums", "math-spectral-norm", "regexp-dna", "string-base64", "string-fasta", "string-tagcloud", "string-unpack-code", "string-validate-input" };
 
             var assembly = Assembly.GetExecutingAssembly();
             Stopwatch sw = new Stopwatch();

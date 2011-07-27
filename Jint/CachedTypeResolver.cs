d7af92d4116d3e169012fe0bc273eb4bd0ea09dd
Sebastien Ros <Sebastien Ros@localhost>
2011-07-27 10:55:52 -0700

    #6909: Fixing thread safety of CachedTypeResolver
    Work Items: 6909

--- Jint/CachedTypeResolver.cs
+++ Jint/CachedTypeResolver.cs
@@ -1,54 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Reflection;
 using System.Threading;
 
 namespace Jint {
     public class CachedTypeResolver : ITypeResolver {
-        Dictionary<string, Type> m_cache = new Dictionary<string, Type>();
-        ReaderWriterLock m_lock = new ReaderWriterLock();
-        static CachedTypeResolver m_default;
+        private readonly Dictionary<string, Type> _cache = new Dictionary<string, Type>();
+        private readonly ReaderWriterLock _lock = new ReaderWriterLock();
+        private static CachedTypeResolver _default;
 
         public static CachedTypeResolver Default {
             get {
                 lock (typeof(CachedTypeResolver)) {
-                    if (m_default == null)
-                        m_default = new CachedTypeResolver();
-                    return m_default;
+                    return _default ?? (_default = new CachedTypeResolver());
                 }
             }
         }
 
         public Type ResolveType(string fullname) {
-            m_lock.AcquireReaderLock(Timeout.Infinite);
+            _lock.AcquireReaderLock(Timeout.Infinite);
 
             try {
-                if (m_cache.ContainsKey(fullname)) {
-                    return m_cache[fullname];
+                if (_cache.ContainsKey(fullname)) {
+                    return _cache[fullname];
                 }
-            }
-            finally {
-                m_lock.ReleaseReaderLock();
-            }
 
-            Type type = null;
-            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies()) {
-                type = a.GetType(fullname, false, false);
+                Type type = null;
+                foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
+                    type = a.GetType(fullname, false, false);
 
-                if (type != null) {
-                    break;
+                    if (type != null) {
+                        break;
+                    }
                 }
-            }
 
-            m_lock.AcquireWriterLock(Timeout.Infinite);
+                _lock.UpgradeToWriterLock(Timeout.Infinite);
 
-            try {
-                m_cache.Add(fullname, type);
+                _cache.Add(fullname, type);
                 return type;
+
             }
             finally {
-                m_lock.ReleaseWriterLock();
+                _lock.ReleaseLock();
             }
         }
     }
